9d0542ebfaa07e623c36b496a4521927998f4da0
Ardi Nugraha <ardi.nugraha@bvarta.com>
2025-05-11 01:15:09 +0700

    feat: add more themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,13 +14,30 @@ return {
 },
         {
   'uloco/bluloco.nvim',
-  lazy = false,
+  lazy = true,
   priority = 1000,
   dependencies = { 'rktjmp/lush.nvim' },
   config = function()
     -- your optional config goes here, see below.
   end,
 },
+{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+{ 
+  'olivercederborg/poimandres.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('poimandres').setup {
+      -- leave this setup function empty for default config
+      -- or refer to the configuration section
+      -- for configuration options
+    }
+  end,
+
+},
+        { "akinsho/horizon.nvim",            lazy = false,
+ version = "*" },
+        { "miikanissi/modus-themes.nvim", priority = 1000 , lazy = false},
     },
  keys = {
       { "<leader>uc", "<cmd>Themery<CR>", desc = "Toggle Themery" },
@@ -29,7 +46,7 @@ return {
     config = function()
       require("themery").setup({
 
-  themes = {"onedark", "bluloco"}
+  themes = {"onedark", "bluloco", "moonfly", "poimandres", "horizon", "modus"}
       })
     end
   }
