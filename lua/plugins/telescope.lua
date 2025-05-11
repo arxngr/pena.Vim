cee1bce29b0e009c30d8cf6b0389d378f1b89e46
ardinugraha <0x4rd1@gmail.com>
2025-05-11 21:46:12 +0700

    feat: make all description is same format

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -80,25 +80,24 @@ return {
         pcall(require('telescope').load_extension, 'ui-select')
 
         -- See `:help telescope.builtin`
-        local builtin = require 'telescope.builtin'
-        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[S]earch [F]iles' })
-        vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+local builtin = require 'telescope.builtin'
 
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Search Help' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Search Keymaps' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Search Files' })
+vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = 'Search Select Telescope' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Search Current Word' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Search by Grep' })
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Search Diagnostics' })
+vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'Search Recent Files ("." for repeat)' })
+vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Find Existing Buffers' })
 
-        -- It's also possible to pass additional configuration options.
-        --  See `:help telescope.builtin.live_grep()` for information about particular keys
-        vim.keymap.set('n', '<leader>sb', function()
-            builtin.live_grep {
-                grep_open_files = true,
-                prompt_title = 'Live Grep in Open Files',
-            }
-        end, { desc = '[S]earch [/] in Open Files' })
+-- Custom live grep for open files
+vim.keymap.set('n', '<leader>sb', function()
+    builtin.live_grep {
+        grep_open_files = true,
+        prompt_title = 'Live Grep in Open Files',
+    }
+end, { desc = 'Search in Open Files' })
     end,
 }
