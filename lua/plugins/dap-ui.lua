2f0462e4fc42a4d5d8cc30f73ea85ba507c2669f
Ardi Nugraha <ardi.nugraha@bvarta.com>
2025-05-19 10:46:19 +0700

    feat: add dapui scopes to the keybind

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -6,13 +6,15 @@ return {
 			"theHamsta/nvim-dap-virtual-text",
 			opts = {
 				enabled = true,
+				highlight_changed_variables = true,
+				all_frames = true,
 				display_callback = function(variable, _buf, _stackframe, _node)
 					local value = variable.value or ""
-					local max_length = 30
+					local max_length = 25
 					if #value > max_length then
 						value = value:sub(1, max_length) .. "..."
 					end
-					return variable.name .. " = " .. value
+					return " = " .. value
 				end,
 			},
 		},
@@ -25,6 +27,7 @@ return {
         { "<leader>dr", function() require("dapui").float_element("repl", { enter = true, position = "center" }) end, desc = "REPL Float" },
         { "<leader>dlb", function() require("dapui").float_element("breakpoints", { enter = true, position = "center" }) end, desc = "Breakpoints Float" },
         { "<leader>dw", function() require("dapui").float_element("watches", { enter = true, position = "center" }) end, desc = "Watches Float" },
+        { "<leader>dv", function() require("dapui").float_element("scopes", { enter = true, position = "center" }) end, desc = "Scopes Float" },
         { "<leader>dt", "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm Float" },
 	},
 	opts = {
