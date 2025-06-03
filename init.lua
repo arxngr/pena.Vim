45da1e8cf286efb4fc11e9440b7f179edee8a3bd
Ardi Nugraha <ardi.nugraha@bvarta.com>
2025-06-03 17:19:16 +0700

    fix: remove aerial

--- init.lua
+++ init.lua
@@ -7,46 +7,45 @@ vim.g.loaded_netrwPlugin = 1
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    require("plugins.oil"),
-    require("plugins.lualine"),
-    require("plugins.treesitter"),
-    require("plugins.cmp"),
-    require("plugins.persistence"),
-    require("plugins.bufferline"),
-    require("plugins.git"),
-    require("plugins.dap"),
-    require("plugins.overseer"),
-    require("plugins.rainbow-delimiter"),
-    require("plugins.conform"),
-    require("plugins.neotest"),
-    require("plugins.trouble"),
-    require("plugins.colorscheme"),
-    require("plugins.visual-multi"),
-    require("plugins.grug-far"),
-    require("plugins.mini"),
-    require("plugins.lsp"),
-    require("plugins.telescope"),
-    require("plugins.terminal"),
-    require("plugins.snacks"),
-    require("plugins.which-key"),
-    require("plugins.noice"),
-    require("plugins.dap-ui"),
-    require("plugins.lush"),
-    require("plugins.edgy"),
-    require("plugins.hbac"),
-    require("plugins.transparent"),
-    require("plugins.wakatime"),
-    require("plugins.aerial")
+	require("plugins.oil"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.cmp"),
+	require("plugins.persistence"),
+	require("plugins.bufferline"),
+	require("plugins.git"),
+	require("plugins.dap"),
+	require("plugins.overseer"),
+	require("plugins.rainbow-delimiter"),
+	require("plugins.conform"),
+	require("plugins.neotest"),
+	require("plugins.trouble"),
+	require("plugins.colorscheme"),
+	require("plugins.visual-multi"),
+	require("plugins.grug-far"),
+	require("plugins.mini"),
+	require("plugins.lsp"),
+	require("plugins.telescope"),
+	require("plugins.terminal"),
+	require("plugins.snacks"),
+	require("plugins.which-key"),
+	require("plugins.noice"),
+	require("plugins.dap-ui"),
+	require("plugins.lush"),
+	require("plugins.edgy"),
+	require("plugins.hbac"),
+	require("plugins.transparent"),
+	require("plugins.wakatime"),
 })
