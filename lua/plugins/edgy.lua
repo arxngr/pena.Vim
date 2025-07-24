4fa5d4fb37e997a4794bb4db734894f93da8a4b0
Ardi Nugraha <ardi.nugraha@bvarta.com>
2025-07-24 13:44:00 +0700

    chore: make toggleterm float and reusable

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -10,34 +10,22 @@ return {
 				enabled = false,
 			},
 			left = {
-				-- {
-				-- 	ft = "trouble",
-				-- 	pinned = true,
-				-- 	title = "Sidebar",
-				-- 	filter = function(_buf, win)
-				-- 		vim.api.nvim_set_hl(0, "TroubleNormal", { bg = "none", ctermbg = "none" })
-				-- 		vim.api.nvim_set_hl(0, "TroubleNormalNC", { bg = "none", ctermbg = "none" })
-				--
-				-- 		local trouble_data = vim.w[win] and vim.w[win].trouble
-				-- 		return trouble_data and trouble_data.mode == "symbols"
-				-- 	end,
-				-- 	open = "Trouble symbols position=left focus=false filter.buf=0",
-				-- 	size = { width = 0.15 },
-				-- },
+				{
+					ft = "trouble",
+					pinned = true,
+					title = "Sidebar",
+					filter = function(_buf, win)
+						vim.api.nvim_set_hl(0, "TroubleNormal", { bg = "none", ctermbg = "none" })
+						vim.api.nvim_set_hl(0, "TroubleNormalNC", { bg = "none", ctermbg = "none" })
+
+						local trouble_data = vim.w[win] and vim.w[win].trouble
+						return trouble_data and trouble_data.mode == "symbols"
+					end,
+					open = "Trouble symbols position=left focus=false filter.buf=0",
+					size = { width = 0.15 },
+				},
 			},
 			bottom = {
-				{
-					title = "ToggleTerm",
-					ft = "toggleterm",
-					size = { height = 0.25 }, -- 23% of total height
-					pinned = false,
-					open = "ToggleTerm direction=horizontal",
-					filter = function(buf, win)
-						-- Only open if not already visible
-
-						return vim.api.nvim_win_get_config(win).relative == ""
-					end,
-				},
 				{
 					title = "DAP REPL",
 					ft = "dap-repl",
