a2be02ccdf53e5dc7faac3631dac73c8e78533f7
ardi-nugraha <0x4rd1@gmail.com>
2025-11-17 12:19:55 +0700

    revert

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -34,16 +34,6 @@ return {
         { "<leader>dlb", function() require("dapui").float_element("breakpoints", { enter = true, position = "center" }) end, desc = "Breakpoints Float" },
         { "<leader>dh",  function() require("dapui").float_element("watches", { enter = true, position = "center" }) end,     desc = "Watches Float" },
         { "<leader>dv",  function() require("dapui").float_element("scopes", { enter = true, position = "center" }) end,      desc = "Scopes Float" },
-        {
-            "<leader>dx",
-            function()
-                local dap = require("dap")
-                dap.terminate()  -- terminates the current debug session
-                vim.notify("DAP session terminated", vim.log.levels.INFO)
-            end,
-            desc = "Terminate DAP",
-        },
-
     },
 	opts = {
 		layouts = {},
