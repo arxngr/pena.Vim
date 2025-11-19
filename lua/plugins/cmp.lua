0c89a1c729b26a7518f3e7af467ab39fe8a25d28
ardinugraha <ardi.nugraha@bvarta.com>
2025-11-19 17:18:08 +0700

    fix: dont override existing keymap

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,43 +1,30 @@
 return {
 	{
-		"L3MON4D3/LuaSnip",
-		keys = function()
-			return {}
-		end,
-	},
-
-	{
 		"hrsh7th/nvim-cmp",
-		dependencies = { "L3MON4D3/LuaSnip" },
 		opts = function(_, opts)
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
 
-			opts.mapping = cmp.mapping.preset.insert({
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump() -- jump to next placeholder
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
+			-- keep existing mappings, just modify or add Tab/S-Tab
+			opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				else
+					fallback()
+				end
+			end, { "i", "s" })
 
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1) -- jump previous placeholder
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-			})
+			opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, { "i", "s" })
 
 			return opts
 		end,
