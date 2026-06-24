ed7958930196020087beea5277e0aa5b7e5d2348
Ardi Nugraha <bvt@bvarta.com>
2026-06-24 16:08:11 +0700

    fix treesitter issue and scheduled cleanup log

--- lua/plugins/syntax/treesitter.lua
+++ lua/plugins/syntax/treesitter.lua
@@ -7,74 +7,40 @@ return {
 			{ "nvim-treesitter/nvim-treesitter-textobjects", lazy = false },
 		},
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"c",
-					"cpp",
-					"asm",
-					"lua",
-					"python",
-					"javascript",
-					"typescript",
-					"vimdoc",
-					"vim",
-					"regex",
-					"terraform",
-					"sql",
-					"dockerfile",
-					"toml",
-					"json",
-					"java",
-					"groovy",
-					"go",
-					"gitignore",
-					"graphql",
-					"yaml",
-					"make",
-					"cmake",
-					"markdown",
-					"markdown_inline",
-					"bash",
-					"tsx",
-					"css",
-					"html",
-				},
-				auto_install = true,
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true,
-						goto_next_start = {
-							["]f"] = "@function.outer",
-							["]c"] = "@class.outer",
-							["]a"] = "@parameter.inner",
-						},
-						goto_next_end = {
-							["]F"] = "@function.outer",
-							["]C"] = "@class.outer",
-							["]A"] = "@parameter.inner",
-						},
-						goto_previous_start = {
-							["[f"] = "@function.outer",
-							["[c"] = "@class.outer",
-							["[a"] = "@parameter.inner",
-						},
-						goto_previous_end = {
-							["[F"] = "@function.outer",
-							["[C"] = "@class.outer",
-							["[A"] = "@parameter.inner",
-						},
-					},
-				},
-			})
+			local langs = {
+				"c", "cpp", "asm", "lua", "python", "javascript", "typescript",
+				"vimdoc", "vim", "regex", "terraform", "sql", "dockerfile",
+				"toml", "json", "java", "groovy", "go", "gitignore", "graphql",
+				"yaml", "make", "cmake", "markdown", "markdown_inline", "bash",
+				"tsx", "css", "html",
+			}
+
+			local ts_ok, ts = pcall(require, "nvim-treesitter.configs")
+			if not ts_ok then ts = require("nvim-treesitter") end
+			ts.setup({ ensure_installed = langs, auto_install = true })
+
+			-- af/if/ac/ic text objects are handled by mini.ai (see plugins/utilities/mini.lua)
+
+			-- movement keymaps via nvim-treesitter-textobjects (silently skip if broken)
+			local mov_ok, mov = pcall(require, "nvim-treesitter.textobjects.move")
+			if mov_ok then
+				local function mv(fn, ...)
+					local args = { ... }
+					return function() pcall(fn, table.unpack(args)) end
+				end
+				vim.keymap.set("n", "]f", mv(mov.goto_next_start,     "@function.outer",  "textobjects"))
+				vim.keymap.set("n", "]c", mv(mov.goto_next_start,     "@class.outer",     "textobjects"))
+				vim.keymap.set("n", "]a", mv(mov.goto_next_start,     "@parameter.inner", "textobjects"))
+				vim.keymap.set("n", "]F", mv(mov.goto_next_end,       "@function.outer",  "textobjects"))
+				vim.keymap.set("n", "]C", mv(mov.goto_next_end,       "@class.outer",     "textobjects"))
+				vim.keymap.set("n", "]A", mv(mov.goto_next_end,       "@parameter.inner", "textobjects"))
+				vim.keymap.set("n", "[f", mv(mov.goto_previous_start, "@function.outer",  "textobjects"))
+				vim.keymap.set("n", "[c", mv(mov.goto_previous_start, "@class.outer",     "textobjects"))
+				vim.keymap.set("n", "[a", mv(mov.goto_previous_start, "@parameter.inner", "textobjects"))
+				vim.keymap.set("n", "[F", mv(mov.goto_previous_end,   "@function.outer",  "textobjects"))
+				vim.keymap.set("n", "[C", mv(mov.goto_previous_end,   "@class.outer",     "textobjects"))
+				vim.keymap.set("n", "[A", mv(mov.goto_previous_end,   "@parameter.inner", "textobjects"))
+			end
 		end,
 	},
 	{
